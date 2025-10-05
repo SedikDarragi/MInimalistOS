8c77d4ae6dc1f29fef375e56df92df415802f202
Sedik Darragi <slimecrafting10@gmail.com>
2025-10-05 01:22:58 +0100

    more fixes

--- boot/minimal_boot.asm
+++ boot/minimal_boot.asm
@@ -3,28 +3,9 @@ ORG 0x7C00
 
 _start:
     jmp 0x0000:start     ; Ensure CS = 0x0000
-    nop
-    
-; BIOS Parameter Block (FAT12 format)
-oem_name          db 'MINIMALOS'
-bytes_per_sector  dw 512
-sectors_per_clust db 1
-reserved_sects    dw 1
-num_fats          db 2
-root_entries      dw 224
-total_sects       dw 2880
-media             db 0xF0
-sectors_per_fat    dw 9
-sectors_per_track  dw 18
-num_heads         dw 2
-hidden_sects      dd 0
-total_sects_large  dd 0
-drive_num         db 0
-reserved          db 0
-boot_sig          db 0x29
-volume_id         dd 0x12345678
-volume_label      db 'MINIMAL OS  '
-file_system       db 'FAT12   '
+
+; Boot disk number
+boot_drive db 0
 
 start:
     ; Basic setup
@@ -35,6 +16,7 @@ start:
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
+    sti
     
     ; Save boot drive (passed from BIOS in DL)
     mov [boot_drive], dl
@@ -43,18 +25,10 @@ start:
     mov ax, 0x0003
     int 0x10
     
-    ; Print boot message
-    mov si, boot_msg
-    call print_string
-    
-    ; Load kernel to 0x100000 (1MB)
-    ; We'll load it in chunks since we're in real mode
-    
-    ; First, reset disk system
-    mov ah, 0x00
-    mov dl, [boot_drive]
-    int 0x13
-    jc disk_error
+    ; Print 'B' in top-left corner
+    mov ah, 0x0E
+    mov al, 'B'
+    int 0x10
     
     ; Set up destination (0x10000 for temporary storage)
     mov ax, 0x1000
@@ -71,15 +45,18 @@ start:
     int 0x13
     jc disk_error
     
-    ; Print success message
-    mov si, read_ok_msg
-    call print_string
+    ; Print 'K' to indicate kernel loaded
+    mov ah, 0x0E
+    mov al, 'K'
+    int 0x10
     
     ; Disable interrupts
     cli
     
     ; Enable A20 line (fast method)
-    call enable_a20
+    in al, 0x92
+    or al, 2
+    out 0x92, al
     
     ; Load GDT
     lgdt [gdt_descriptor]
@@ -90,7 +67,7 @@ start:
     mov cr0, eax
     
     ; Far jump to 32-bit code to flush the pipeline
-    jmp CODE_SEG:protected_mode
+    jmp 0x08:protected_mode
     
 enable_a20:
     ; Try fast A20 method first
@@ -128,115 +105,61 @@ print_string:
     popa
     ret
 
-; Print newline
-print_nl:
+; Print byte in AL as hex
+print_hex_byte:
     pusha
-    mov si, crlf
-    call print_string
-    popa
-    ret
-
-; Print 32-bit value in EAX (in hex)
-print_hex:
-    pusha
-    mov ecx, 8        ; 8 hex digits
-    mov edi, hex_buf + 1
-
-.digit_loop:
-    mov edx, eax
-    and edx, 0x0F
-    cmp dl, 9
-    jbe .is_digit
-    add dl, 7         ; 'A' - '0' - 10
+    mov bl, al
+    mov cx, 2
+    mov ah, 0x0E
+.next_nibble:
+    rol bl, 4
+    mov al, bl
+    and al, 0x0F
+    cmp al, 0x0A
+    jl .is_digit
+    add al, 7        ; 'A' - '0' - 10
 .is_digit:
-    add dl, '0'
-    mov [edi + ecx - 1], dl
-    shr eax, 4
-    loop .digit_loop
-
-    mov si, hex_buf
-    call print_string
+    add al, '0'
+    int 0x10
+    loop .next_nibble
     popa
     ret
 
 ; Error handler
 disk_error:
-    mov si, error_msg
+    mov si, .err_msg
     call print_string
     mov al, ah
     call print_hex_byte
-    mov si, crlf
-    call print_string
     cli
 .hang:
     hlt
     jmp .hang
-
-print_hex_byte:
-    pusha
-    mov bl, al
-    shr al, 4
-    call .nibble
-    mov al, bl
-    and al, 0x0F
-    call .nibble
-    popa
-    ret
-.nibble:
-    add al, '0'
-    cmp al, '9'
-    jbe .print
-    add al, 7
-.print:
-    call print_char
-    ret
+.err_msg db 'E', 0
 
 BITS 32
 protected_mode:
     ; Set up segment registers for flat memory model
-    mov ax, 0x10        ; Data segment selector (0x10 points to our data segment in GDT)
+    mov ax, 0x10        ; Data segment selector
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    mov esp, 0x90000    ; Set up stack at 0x90000 (grows down)
+    mov esp, 0x90000    ; Set up stack at 0x90000
     
-    ; Clear direction flag (string operations increment)
-    cld
+    ; Print 'K' in top-left corner
+    mov byte [0xB8000], 'K'
+    mov byte [0xB8001], 0x1F  ; White on blue
     
-    ; Clear screen with blue background
-    mov edi, 0xB8000    ; VGA text buffer
-    mov ecx, 80*25      ; 80x25 characters
-    mov eax, 0x1F201F20 ; Blue background, white spaces (two characters at a time)
-    rep stosd
-    
-    ; Print message
-    mov esi, pm_msg
-    mov edi, 0xB8000 + (80 * 2 + 20) * 2  ; Row 2, column 20
-    mov ah, 0x1F        ; White on blue
-.print_loop:
-    lodsb
-    or al, al           ; Check for null terminator
-    jz .copy_kernel
-    stosw               ; Write character and attribute
-    jmp .print_loop
-    
-.copy_kernel:
-    ; Copy kernel from 0x10000 to 0x100000 (1MB)
+    ; Copy kernel to 1MB
     mov esi, 0x10000    ; Source (where we loaded the kernel)
     mov edi, 0x100000   ; Destination (1MB)
-    mov ecx, 0x2000     ; Copy 8KB (16 sectors * 512 bytes)
-    rep movsd           ; Copy dwords (faster than bytes)
+    mov ecx, 0x2000     ; 8KB (16 sectors * 512 bytes)
+    rep movsd
     
-    ; Jump to the kernel
-    jmp 0x08:0x100000   ; Use code segment selector with proper privilege level
-    
-    ; Halt if we return (shouldn't happen)
-    cli
-.hang:
-    hlt
-    jmp .hang
+    ; Jump to kernel
+    jmp 0x08:0x100000
 
 ; GDT
 gdt_start:
@@ -271,15 +194,7 @@ gdt_descriptor:
     CODE_SEG equ 0x08    ; 0x08 = 00001 0 00 (index 1, GDT, RPL 0)
     DATA_SEG equ 0x10    ; 0x10 = 00010 0 00 (index 2, GDT, RPL 0)
 
-; Messages
-boot_msg        db 'Booting...', 0x0D, 0x0A, 0
-read_ok_msg     db 'Kernel loaded', 0x0D, 0x0A, 0
-error_msg       db 'Error: 0x', 0
-crlf            db 0x0D, 0x0A, 0
-pm_msg          db 'PM', 0x0D, 0x0A, 0
-hex_buf         db '0x00000000', 0
 
-boot_drive db 0
-
+; Pad to 510 bytes and add boot signature
 times 510-($-$$) db 0
 dw 0xAA55
