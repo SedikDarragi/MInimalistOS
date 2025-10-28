1bada2222f18306ecc15457aa062f61ef5a4124c
Sedik Darragi <slimecrafting10@gmail.com>
2025-10-28 11:17:38 +0100

    more fixes

--- boot/minimal_boot_new.asm
+++ boot/minimal_boot_new.asm
@@ -8,19 +8,21 @@ start:
     mov ss, ax
     mov sp, 0x7C00
     
-    ; Load kernel directly to 1MB (0x100000)
+    ; Load kernel to 0x10000 (64KB)
     mov ax, 0x1000
     mov es, ax
     xor bx, bx
-    mov ah, 0x02    ; Read sectors
-    mov al, 1       ; 1 sector only
-    xor ch, ch      ; Cylinder 0
-    mov cl, 2       ; Sector 2 (1-based)
-    xor dh, dh      ; Head 0
+    
+    ; Read kernel (16 sectors = 8KB)
+    mov ah, 0x02
+    mov al, 16
+    xor ch, ch
+    mov cl, 2
+    xor dh, dh
     int 0x13
     jc $
     
-    ; Enable A20 (fast method)
+    ; Enable A20
     in al, 0x92
     or al, 2
     out 0x92, al
@@ -35,35 +37,23 @@ start:
 
 ; Minimal GDT
 gdt:
-    ; Null descriptor
-    dw 0, 0, 0, 0
-    
-    ; Code segment (0x08)
-    dw 0xFFFF    ; Limit 0-15
-    dw 0x0000    ; Base 0-15
-    db 0x00      ; Base 16-23
-    db 0x9A      ; Present, Ring 0, Code, Execute/Read
-    db 0xCF      ; 4K granularity, 32-bit
-    db 0x00      ; Base 24-31
+    dq 0
+    dw 0xFFFF, 0, 0x9A00, 0x00CF  ; Code
 
 gdt_desc:
-    dw 0x17      ; GDT size - 1
-    dd gdt       ; GDT address
+    dw 0x0F
+    dd gdt
 
-times 8 db 0     ; Padding to align the protected mode code
+times 446-($-$$) db 0
+dw 0xAA55
 
 [BITS 32]
 pm_start:
-    ; Set up segment registers
+    ; Set up segments
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, 0x90000
     
-    ; Jump to kernel at 1MB
-    jmp 0x08:0x100000
-
-; Fill to 510 bytes and add boot signature
-times 510-($-$$) db 0
-dw 0xAA55
+    ; Jump to kernel at 0x10000
+    jmp 0x08:0x10000
