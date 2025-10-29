82a49e1aaaa72d43c57398eaacabfdcebd80d0d1
Sedik Darragi <slimecrafting10@gmail.com>
2025-10-29 19:18:46 +0100

    booting issues

--- boot/minimal_boot_new.asm
+++ boot/minimal_boot_new.asm
@@ -1,10 +1,6 @@
 [BITS 16]
 [ORG 0x7C00]
 
-; Data
-boot_drive db 0
-attempt db '0'
-
 start:
     cli
     xor ax, ax
@@ -13,25 +9,14 @@ start:
     mov sp, 0x7C00
     mov [boot_drive], dl
 
-    ; Print 'S'
-    mov ah, 0x0E
-    mov al, 'S'
-    int 0x10
-
-    ; Reset disk
-    xor ah, ah
-    mov dl, [boot_drive]
-    int 0x13
-    jc error
-
     ; Set up destination (0x1000:0x0000 = 0x10000)
     mov ax, 0x1000
     mov es, ax
     xor bx, bx
 
-    ; Read kernel
+    ; Read kernel (single sector)
     mov ah, 0x02
-    mov al, 8
+    mov al, 1
     mov ch, 0
     mov cl, 2
     mov dh, 0
@@ -43,35 +28,13 @@ start:
     jmp 0x1000:0x0000
 
 error:
-    ; Print 'E' and error code
+    ; Simple error indicator
     mov ah, 0x0E
     mov al, 'E'
     int 0x10
-    mov al, ah
-    call print_hex
-    hlt
+    jmp $
 
-print_hex:
-    push ax
-    mov cl, 4
-    shr al, cl
-    call print_digit
-    pop ax
-    and al, 0x0F
-    call print_digit
-    ret
-
-print_digit:
-    push ax
-    cmp al, 10
-    jb .digit
-    add al, 7
-.digit:
-    add al, '0'
-    mov ah, 0x0E
-    int 0x10
-    pop ax
-    ret
+boot_drive db 0
 
 ; Pad to 510 bytes and add boot signature
 times 510-($-$$) db 0
