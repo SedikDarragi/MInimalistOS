ad92a7f3c533295d05f2b799ca22619f9a846df7
Sedik Darragi <slimecrafting10@gmail.com>
2025-10-30 18:41:46 +0100

    booting fixes

--- boot/minimal_boot_new.asm
+++ boot/minimal_boot_new.asm
@@ -7,33 +7,25 @@ start:
     mov ds, ax
     mov ss, ax
     mov sp, 0x7C00
-    push dx              ; Save boot drive
-
-    ; Set up destination (0x1000:0x0000 = 0x10000)
+    push dx
     mov ax, 0x1000
     mov es, ax
     xor bx, bx
-
-    ; Read kernel (single sector)
     mov ah, 0x02
     mov al, 1
     xor ch, ch
     mov cl, 2
     xor dh, dh
-    pop dx               ; Restore boot drive
+    pop dx
     int 0x13
     jc error
-
-    ; Jump to kernel
     jmp 0x1000:0x0000
 
 error:
-    ; Simple error indicator
     mov ah, 0x0E
     mov al, 'E'
     int 0x10
     jmp $
 
-; Pad to 510 bytes and add boot signature
-times 510-($-$$) db 0
-dw 0xAA55
+times 508-($-$$) db 0
+db 0x55, 0xAA
