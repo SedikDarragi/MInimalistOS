e67f59cc21667a87937f1d7a899e0b7c760cb066
Sedik Darragi <slimecrafting10@gmail.com>
2025-11-01 22:14:53 +0100

    booting issues

--- boot/minimal_boot_new.asm
+++ boot/minimal_boot_new.asm
@@ -1,31 +1,48 @@
 [BITS 16]
 [ORG 0x7C00]
 
-start:
-    cli
-    xor ax, ax
-    mov ds, ax
-    mov ss, ax
-    mov sp, 0x7C00
-    push dx
-    mov ax, 0x1000
-    mov es, ax
-    xor bx, bx
-    mov ah, 0x02
-    mov al, 1
-    xor ch, ch
-    mov cl, 2
-    xor dh, dh
-    pop dx
-    int 0x13
-    jc error
-    jmp 0x1000:0x0000
+jmp 0x0000:main
+
+main:
+cli
+xor ax, ax
+mov ds, ax
+mov ss, ax
+mov sp, 0x7C00
+sti
+
+; Save boot drive
+mov [0x7E00], dl
+
+; Reset disk system
+xor ax, ax
+mov dl, [0x7E00]
+int 0x13
+jc error
+
+; Load kernel to 0x8000 (512-byte aligned)
+mov ax, 0x800
+mov es, ax
+xor bx, bx
+
+; Read 4 sectors (2KB) starting from sector 2
+mov ah, 0x02
+mov al, 4
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, [0x7E00]
+int 0x13
+jc error
+
+; Jump to kernel
+jmp 0x800:0x0000
 
 error:
-    mov ah, 0x0E
-    mov al, 'E'
-    int 0x10
-    jmp $
+mov ax, 0x0E45
+int 0x10
+hlt
+jmp $
 
 times 508-($-$$) db 0
-db 0x55, 0xAA
+dw 0xAA55
