42f0ab0574230ab0aebb714d1323fc7171dfaabe
Sedik Darragi <slimecrafting10@gmail.com>
2025-11-13 19:19:39 +0100

    fixes

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stddef.h> // For memset
 #include "idt.h"
 
--- kernel/irq.asm
+++ kernel/irq.asm
@@ -1,37 +1,136 @@
 ; IRQ handlers
 [bits 32]
 
-global irq0
+; External C handlers
+extern fault_handler
+extern irq_handler
 
-extern default_irq_handler
-
-%macro IRQ 1
-global irq%1
-irq%1:
-    cli
-    pusha
-    push %1
-    call default_irq_handler
-    add esp, 4
-    popa
-    sti
-    iret
+; Macro for ISRs that don't push an error code (exceptions 0-7, 9, 15+)
+%macro ISR_NOERRCODE 1
+    global isr%1
+    isr%1:
+        cli
+        push 0  ; Push a dummy error code
+        push %1 ; Push the interrupt number
+        jmp isr_common_stub
 %endmacro
 
-; Define IRQ handlers
-IRQ 0  ; Timer
-IRQ 1  ; Keyboard
-IRQ 2  ; Cascade
-IRQ 3  ; COM2
-IRQ 4  ; COM1
-IRQ 5  ; LPT2
-IRQ 6  ; Floppy
-IRQ 7  ; LPT1
-IRQ 8  ; CMOS RTC
-IRQ 9  ; Free
-IRQ 10 ; Free
-IRQ 11 ; Free
-IRQ 12 ; PS/2 Mouse
-IRQ 13 ; FPU
-IRQ 14 ; Primary ATA
-IRQ 15 ; Secondary ATA
+; Macro for ISRs that do push an error code (exceptions 8, 10-14)
+%macro ISR_ERRCODE 1
+    global isr%1
+    isr%1:
+        cli
+        ; Error code is already on the stack
+        push %1 ; Push the interrupt number
+        jmp isr_common_stub
+%endmacro
+
+; Macro for IRQs (hardware interrupts)
+%macro IRQ 2
+    global irq%1
+    irq%1:
+        cli
+        push 0  ; Push a dummy error code
+        push %2 ; Push the interrupt number (32 + IRQ number)
+        jmp irq_common_stub
+%endmacro
+
+; Common stub for ISRs
+isr_common_stub:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10 ; Load kernel data segment
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov eax, esp ; Pass pointer to stack to C handler
+    push eax
+    call fault_handler
+    pop eax
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8 ; Clean up error code and interrupt number
+    iret
+
+; Common stub for IRQs
+irq_common_stub:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    mov ax, 0x10 ; Load kernel data segment
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov eax, esp ; Pass pointer to stack to C handler
+    push eax
+    call irq_handler
+    pop eax
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8 ; Clean up error code and interrupt number
+    iret
+
+; --- ISRs ---
+ISR_NOERRCODE 0
+ISR_NOERRCODE 1
+ISR_NOERRCODE 2
+ISR_NOERRCODE 3
+ISR_NOERRCODE 4
+ISR_NOERRCODE 5
+ISR_NOERRCODE 6
+ISR_NOERRCODE 7
+ISR_ERRCODE   8
+ISR_NOERRCODE 9
+ISR_ERRCODE   10
+ISR_ERRCODE   11
+ISR_ERRCODE   12
+ISR_ERRCODE   13
+ISR_ERRCODE   14
+ISR_NOERRCODE 15
+ISR_NOERRCODE 16
+ISR_NOERRCODE 17
+ISR_NOERRCODE 18
+ISR_NOERRCODE 19
+ISR_NOERRCODE 20
+ISR_NOERRCODE 21
+ISR_NOERRCODE 22
+ISR_NOERRCODE 23
+ISR_NOERRCODE 24
+ISR_NOERRCODE 25
+ISR_NOERRCODE 26
+ISR_NOERRCODE 27
+ISR_NOERRCODE 28
+ISR_NOERRCODE 29
+ISR_NOERRCODE 30
+ISR_NOERRCODE 31
+
+; --- IRQs ---
+IRQ 0, 32
+IRQ 1, 33
+IRQ 2, 34
+IRQ 3, 35
+IRQ 4, 36
+IRQ 5, 37
+IRQ 6, 38
+IRQ 7, 39
+IRQ 8, 40
+IRQ 9, 41
+IRQ 10, 42
+IRQ 11, 43
+IRQ 12, 44
+IRQ 13, 45
+IRQ 14, 46
+IRQ 15, 47
