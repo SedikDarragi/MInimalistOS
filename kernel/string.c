97aa0cbf14a86b1f33b4a840b026ab9c11acda4f
Sedik Darragi <slimecrafting10@gmail.com>
2025-11-13 19:24:32 +0100

    fixes

---
+++ kernel/string.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+
+// A simple implementation of memset
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = s;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
+    return s;
+}
