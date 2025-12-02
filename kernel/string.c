2e47716c456631dfc7beca8625a17401eb39dec6
Sedik Darragi <slimecrafting10@gmail.com>
2025-12-02 23:37:46 +0100

    changes

--- kernel/string.c
+++ kernel/string.c
@@ -8,3 +8,37 @@ void *memset(void *s, int c, size_t n) {
     }
     return s;
 }
+
+// String copy function
+char *strcpy(char *dest, const char *src) {
+    char *d = dest;
+    while ((*d++ = *src++) != '\0');
+    return dest;
+}
+
+// String comparison function
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
+
+// String concatenation function
+char *strcat(char *dest, const char *src) {
+    char *d = dest;
+    while (*d) d++;
+    while ((*d++ = *src++) != '\0');
+    return dest;
+}
+
+// Memory copy function
+void *memcpy(void *dest, const void *src, size_t n) {
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
