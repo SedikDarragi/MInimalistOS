26b4ff65ba98982ff5c5283f1993f152c3f9e38e
Sedik Darragi <slimecrafting10@gmail.com>
2026-03-15 11:18:23 +0100

    fixes

--- kernel/shell.c
+++ kernel/shell.c
@@ -9,15 +9,14 @@ command_history_t history;
 
 // External dependency for keyboard input (assuming standard driver naming)
 extern char keyboard_getchar(void);
+extern void serial_putchar(uint16_t com, char c);
 
 // Serial I/O helpers
-static void serial_putc(char c) {
-    while ((inb(0x3F8 + 5) & 0x20) == 0); // Wait for transmit empty
-    outb(0x3F8, c);
-}
-
 static void serial_print(const char* str) {
-    while (*str) serial_putc(*str++);
+    while (*str) {
+        if (*str == '\n') serial_putchar(0x3F8, '\r');
+        serial_putchar(0x3F8, *str++);
+    }
 }
 
 static char shell_serial_getchar(void) {
