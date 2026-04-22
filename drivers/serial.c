b66d45280fa0169bd62fcd317efeb8178bd95379
Sedik Darragi <slimecrafting10@gmail.com>
2026-04-22 21:08:47 +0100

    fixes

--- drivers/serial.c
+++ drivers/serial.c
@@ -47,19 +47,17 @@ int serial_init(void) {
 }
 
 // Check if transmit buffer is empty
-static int serial_is_transmit_empty(void) {
-    return inb(SERIAL_LINE_STATUS_PORT(SERIAL_COM1_BASE)) & 0x20;
+static int serial_is_transmit_empty(uint16_t com) {
+    return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
 }
 
 // Write character to serial port
-void serial_putchar(char c) {
+void serial_putchar(uint16_t com, char c) {
     // Add timeout to prevent infinite hang if serial port is stuck
-    uint16_t com = SERIAL_COM1_BASE;
     int timeout = 100000;
-    while (serial_is_transmit_empty() == 0) {
+    while (serial_is_transmit_empty(com) == 0) {
         if (timeout-- <= 0) break;
     }
-    
     outb(SERIAL_DATA_PORT(com), c);
 }
 
